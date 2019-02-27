2389b7cd613e07fafe1f9139c6bd3274f568a3e6
Hank Ehly <henry.ehly@gmail.com>
2019-02-27 22:16:54 +0900

    Add MZA template

---
+++ jrdb/datadocs/MZA.py
@@ -0,0 +1,13 @@
+from jrdb.datadocs.template import Template
+
+
+class MZA(Template):
+    """
+    http://www.jrdb.com/program/Msa/msa_doc.txt
+    """
+    name = '抹消馬'
+    items = [
+        ['pedigree_registration_number', '血統登録番号', None, '8', 'X', '1', None],
+        ['reserved', '予備', None, '11', 'X', '292', 'スペース'],
+        ['newline', '改行', None, '2', 'X', '303', 'ＣＲ・ＬＦ']
+    ]
--- jrdb/datadocs/__init__.py
+++ jrdb/datadocs/__init__.py
@@ -1,6 +1,7 @@
 from .CZA import CZA
 from .KYI import KYI
 from .KZA import KZA
+from .MZA import MZA
 from .SED import SED
 from .SKB import SKB
 from .SRB import SRB
