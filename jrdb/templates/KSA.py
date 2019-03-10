2a6a4e276de24a64bed7e9ea990389131f724035
Hank Ehly <henry.ehly@gmail.com>
2019-03-10 10:25:00 +0900

    Add KSA template

---
+++ jrdb/templates/KSA.py
@@ -0,0 +1,11 @@
+from jrdb.templates import KZA
+
+
+class KSA(KZA):
+    """
+    JRDB騎手データ（KSA）
+    差分(今週出走分,先週成績更新分)
+
+    http://www.jrdb.com/program/Ks/Ks_doc1.txt
+    """
+    name = 'JRDB騎手データ（KSA）'
