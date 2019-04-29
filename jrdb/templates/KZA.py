3d8fe92d4862bd787c7c15ed5dda4ac8ef2c6052
Hank Ehly <henry.ehly@gmail.com>
2019-04-29 11:12:23 -0500

    Refactor KZA persist

--- jrdb/templates/KZA.py
+++ jrdb/templates/KZA.py
@@ -1,16 +1,15 @@
 import logging
 
-from django.db import IntegrityError, transaction, connection
 from django.utils.functional import cached_property
 
-from ..models import Jockey, Trainer, choices
-from .template import Template, startswith
+from ..models import choices
+from .template import Template, ModelPersistMixin
 from .item import StringItem, DateItem, ChoiceItem, IntegerItem, ArrayItem
 
 logger = logging.getLogger(__name__)
 
 
-class KZA(Template):
+class KZA(Template, ModelPersistMixin):
     """
     http://www.jrdb.com/program/Ks/Ks_doc1.txt
     """
@@ -51,43 +50,4 @@ class KZA(Template):
         return super().clean
 
     def persist(self):
-        df = self.clean.pipe(startswith, 'jockey__', rename=True)
-
-        cols = ','.join('"{}"'.format(key) for key in df.columns)
-        # TODO: Handle null transformations more elegantly
-        vals = ','.join(map(str, map(tuple, df.values))) \
-            .replace('nan', 'NULL') \
-            .replace('None', 'NULL') \
-            .replace('\'NaT\'', 'NULL')
-        updates = ','.join((f'{key}=excluded.{key}' for key in df.columns))
-
-        sql = (
-            f'INSERT INTO jockeys ({cols}) '
-            f'VALUES {vals} '
-            f'ON CONFLICT (code) '
-            f'DO UPDATE SET {updates} '
-            f'WHERE excluded.jrdb_saved_on >= jockeys.jrdb_saved_on'
-        )
-
-        with connection.cursor() as c:
-            c.execute(sql)
-
-    # @transaction.atomic
-    # def persist(self):
-    # for _, row in self.clean.iterrows():
-    #     try:
-    #         j = row.pipe(startswith, 'jockey__', rename=True).dropna().to_dict()
-    #
-    #         if row.trainer__code:
-    #             trainer, _ = Trainer.objects.get_or_create(code=row.trainer__code)
-    #             j['trainer_id'] = trainer.id
-    #
-    #         jockey, created = Jockey.objects.get_or_create(code=j.pop('code'), defaults=j)
-    #
-    #         if not created:
-    #             if jockey.jrdb_saved_on is None or j['jrdb_saved_on'] >= jockey.jrdb_saved_on:
-    #                 for name, value in j.items():
-    #                     setattr(jockey, name, value)
-    #                 jockey.save()
-    #     except IntegrityError as e:
-    #         logger.exception(e)
+        self.persist_model('jrdb.Jockey', conflict_condition='excluded.jrdb_saved_on >= jockeys.jrdb_saved_on')
