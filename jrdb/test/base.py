dd46772418d0d8b9717527c5d8e3250614941fbf
Hank Ehly <henry.ehly@gmail.com>
2019-05-02 10:30:13 -0500

    Add arg type names to assertion helper error msg

--- jrdb/test/base.py
+++ jrdb/test/base.py
@@ -5,5 +5,10 @@ class JRDBTestCase(TestCase):
 
     def assertSubDict(self, sub, sup):
         diff = [key for key in sub if (key in sup and not sup[key] == sub[key])]
-        msg = ' , '.join([f"{key} <subset: {sub[key]}, superset: {sup[key]}>" for key in diff])
+        msg = ' , '.join([
+            f'{key} <'
+            f'subset: {sub[key]} ({type(sub[key]).__name__}), '
+            f'superset: {sup[key]} ({type(sup[key]).__name__})'
+            f'>' for key in diff
+        ])
         return self.assertTrue(sub.items() <= sup.items(), msg)
