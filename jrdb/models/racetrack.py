67962bf7ebc382c893d0954a97e6f73e158401f9
Hank Ehly <henry.ehly@gmail.com>
2019-05-07 07:54:15 +0900

    Add english racetrack names

--- jrdb/models/racetrack.py
+++ jrdb/models/racetrack.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Racetrack(models.Model):
-    # TODO: english racetrack name
     code = models.CharField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
+    name_en = models.CharField(max_length=255)
 
     class Meta:
         db_table = 'racetracks'
